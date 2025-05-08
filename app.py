38d54fb8d6502abdd56e51161dd0f257799f457d
Kjetil <kjefriborg@gmail.com>
2025-05-08 16:19:01 +0200

    First development

--- app.py
+++ app.py
@@ -15,11 +15,16 @@ db = mysql.connector.connect(
 cursor = db.cursor(dictionary=True)
 
 @app.route('/')
+@app.route('/createEmployee')
 def form():
     return render_template('createEmployee.html')
 
-@app.route('/createPerson', methods=['POST'])
-def createPerson():
+@app.route('/createShift')
+def createShiftPage():
+    return render_template('createShift.html')
+
+@app.route('/createEmployee', methods=['POST'])
+def createEmployee():
     Name = request.form['Name']
     PositionPercent = request.form['PositionPercent']
 
@@ -34,20 +39,14 @@ def viewEmployees():
     employees = cursor.fetchall()
     return render_template('viewEmployees.html', employees=employees)
 
-@app.route('/viewShifts')
-def viewShifts():
-    cursor.execute("SELECT * FROM shifts")
-    shifts = cursor.fetchall()
-    return render_template('viewShifts.html', shifts=shifts)
-
-@app.route('/editEmployees/<int:id>')
-def edit(id):
+@app.route('/editEmployee/<int:id>')
+def editEmployee(id):
     cursor.execute("SELECT * FROM employees WHERE id = %s", (id,))
     employee = cursor.fetchone()
-    return render_template('editEmployees.html', employee=employee)
+    return render_template('editEmployee.html', employee=employee)
 
 @app.route('/updateEmployee/<int:Id>', methods=['POST'])
-def update(Id):
+def updateEmployee(Id):
     Name = request.form['Name']
     PositionPercent = request.form['PositionPercent']
 
@@ -59,7 +58,43 @@ def update(Id):
     db.commit()
     return redirect('/viewEmployees')
 
+@app.route('/createShift', methods=['POST'])
+def createShift():
+    Name = request.form['Name']
+    StartTime = request.form['StartTime']
+    EndTime = request.form['EndTime']
+    Length = request.form['Length']
 
+    cursor.execute("INSERT INTO employees (Name, StartTime, EndTime, Length) VALUES (%s, %s, %s, %s)", (Name, StartTime, EndTime, Length))
+    db.commit()
+    return redirect('/viewShifts.html')
+
+@app.route('/viewShifts')
+def viewShifts():
+    cursor.execute("SELECT * FROM shifts")
+    shifts = cursor.fetchall()
+    return render_template('viewShifts.html', shifts=shifts)
+
+@app.route('/editShifts/<int:id>')
+def editShifts(id):
+    cursor.execute("SELECT * FROM shifts WHERE id = %s", (id,))
+    shift = cursor.fetchone()
+    return render_template('editShifts.html', shift=shift)
+
+@app.route('/updateShift/<int:Id>', methods=['POST'])
+def updateShift(Id):
+    Name = request.form['Name']
+    StartTime = request.form['StartTime']
+    EndTime = request.form['EndTime']
+    Length = request.form['Length']
+
+    cursor.execute("""
+        UPDATE shifts 
+        SET Name = %s, StartTime = %s, EndTime = %s, Length = %s 
+        WHERE Id = %s
+    """, (Name, StartTime, EndTime, Length, Id))
+    db.commit()
+    return redirect('/viewShifts')
 
 
 if __name__ == '__main__':
