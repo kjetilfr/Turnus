695cccf09a53f725468fecd7e2b96c4cc12406ec
Kjetil <kjefriborg@gmail.com>
2025-10-28 21:48:46 +0100

    fixed redirect

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -15,7 +15,7 @@ export async function GET(request: Request) {
       if (isLocalEnv) {
         return NextResponse.redirect(`${origin}${next}`)
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+        return NextResponse.redirect(`https://turnus-hjelp.no/${next}`)
       } else {
         return NextResponse.redirect(`${origin}${next}`)
       }
